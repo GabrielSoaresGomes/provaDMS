06410dfd4bfef7ceb9aba4e885966749ce73db7a
PuppetTheDragon <gabrielsoares221@gmail.com>
2021-10-29 12:33:34 -0300

    Tentativa de adicionar datas

--- aluguel/models.py
+++ aluguel/models.py
@@ -1,21 +1,12 @@
 from django.db import models
 
-
 class Estadio(models.Model):
-    CIDADE_CHOICES = (
-        ("Valença", "Valença"),
-        ("Barra do Piraí", "Barra do Piraí"),
-        ("Paracambi", "Paracambi"),
-        ("Vassouras", "Vassouras"),
-    )
-
-    cidade = models.CharField('Cidade', choices=CIDADE_CHOICES, max_length=40)
-    nomeEstadio = models.CharField('Nome do Estadio', max_length=50)
-    donoCampo = models.CharField('Dono do Campo', max_length=80)
-    enderecoCampo = models.CharField('Endereço do Campo', max_length=150)
-    comprimentoCampo = models.SmallIntegerField('Comprimento do Campo')
-    larguraCampo = models.SmallIntegerField('Largura do Campo')
-    precoHora = models.PositiveSmallIntegerField('Preço por Hora')
-
+    cidade = models.CharField(max_length=40)
+    nomeClube = models.CharField(max_length=50)
+    donoCampo = models.CharField(max_length=80)
+    enderecoCampo = models.CharField(max_length=150)
+    comprimentoCampo = models.SmallIntegerField()
+    larguraCampo = models.SmallIntegerField()
+    precoHora = models.PositiveSmallIntegerField()
     def __str__(self):
         return self.cidade
