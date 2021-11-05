2cd0fb760796edf66556a673b83cb2c19fddf40f
PuppetTheDragon <gabrielsoares221@gmail.com>
2021-11-05 12:53:59 -0300

    Adicionando os templates de  blocks  do django

--- aluguel/models.py
+++ aluguel/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Estadio(models.Model):
 
-    cidade = models.CharField('Cidade', max_length=40, default="oi")
-    nomeEstadio = models.CharField('Nome do Estadio', max_length=50, default="oi")
-    donoCampo = models.CharField('Dono do Campo', max_length=80, default="oi")
-    enderecoCampo = models.CharField('Endereço do Campo', max_length=150, default="oi")
-    comprimentoCampo = models.SmallIntegerField('Comprimento do Campo')
-    larguraCampo = models.SmallIntegerField('Largura do Campo')
-    precoHora = models.PositiveSmallIntegerField('Preço por Hora')
+    cidade = models.CharField('Cidade', max_length=40)
+    nomeEstadio = models.CharField('Nome do Estadio', max_length=50)
+    donoCampo = models.CharField('Dono do Campo', max_length=80)
+    enderecoCampo = models.CharField('Endereço do Campo', max_length=150)
+    comprimentoCampo = models.SmallIntegerField('Comprimento do Campo (metros)')
+    larguraCampo = models.SmallIntegerField('Largura do Campo (metros)')
+    precoHora = models.PositiveSmallIntegerField('Preço por Hora (R$)')
 
     def __str__(self):
         return self.cidade
--- aluguel/views.py
+++ aluguel/views.py
@@ -3,30 +3,6 @@ from .models import Estadio, PrecoFinal
 from .forms import EstadioForm, PrecoFinalForm
 
 
-def listaEstadios(request):
-    estadios = Estadio.objects.all()
-    return render(request, "estadios.html", {"estadios": estadios})
-
-
-def atualizarEstadio(request, id):
-    estadio = Estadio.objects.get(id=id)
-    form = EstadioForm(request.POST or None, instance=estadio)
-
-    if form.is_valid():
-        form.save()
-        return redirect('listaEstadios')
-    return render(request, "estadioForm.html", {"form": form, "estadio": estadio})
-
-
-def deletarEstadio(request, id):
-    estadio = Estadio.objects.get(id=id)
-
-    if request.method == "POST":
-        estadio.delete()
-        return redirect('listaEstadios')
-    return render(request, 'estadioDeleteConfirm.html', {'estadio': estadio})
-
-
 def adicionarEstadio(request):
     form = EstadioForm(request.POST or None)
 
@@ -45,10 +21,44 @@ def escolherEstadio(request, id):
     return render(request, 'precoFinal.html', {"form": form, "estadio": estadio})
 
 
+def listaEstadios(request):
+    estadios = Estadio.objects.all()
+    return render(request, "estadios.html", {"estadios": estadios})
+
+
 def estadiosEscolhidos(request):
     estadios = PrecoFinal.objects.all()
 
     return render(request, "estadiosAgendados.html", {"estadios": estadios})
+
+
+def atualizarEstadio(request, id):
+    estadio = Estadio.objects.get(id=id)
+    form = EstadioForm(request.POST or None, instance=estadio)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listaEstadios')
+    return render(request, "estadioForm.html", {"form": form, "estadio": estadio})
+
+
+def atualizarEscolhido(request, id):
+    alugado = PrecoFinal.objects.get(id=id)
+    form = PrecoFinalForm(request.POST or None, instance=alugado)
+
+    if form.is_valid():
+        form.save()
+        return redirect('estadiosEscolhidos')
+    return render(request, "precoFinal.html", {"form": form, "alugado": alugado})
+
+
+def deletarEstadio(request, id):
+    estadio = Estadio.objects.get(id=id)
+
+    if request.method == "POST":
+        estadio.delete()
+        return redirect('listaEstadios')
+    return render(request, 'estadioDeleteConfirm.html', {'estadio': estadio})
 
 
 def deletarEscolhido(request, id):
